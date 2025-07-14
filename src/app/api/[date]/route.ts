efc27e30b1dac2d2c888e6ebd61705725631ec63
Fabricio Carvalho <fcarvalho1999@gmail.com>
2025-07-14 12:11:05 -0300

    fix api call

--- src/app/api/[date]/route.ts
+++ src/app/api/[date]/route.ts
@@ -7,9 +7,12 @@ interface RouteParams {
 	};
 }
 
-export async function DELETE(request: NextRequest, { params }: RouteParams) {
+export async function DELETE(
+	request: NextRequest,
+	{ params }: { params: Promise<{ date: string }> }
+) {
 	try {
-		const { date } = params;
+		const { date } = await params; // Note o await aqui
 		await pool.query("DELETE FROM workouts_log WHERE log_date = $1", [date]);
 		return NextResponse.json({ message: "Treino removido" });
 	} catch (error) {
